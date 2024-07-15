2618b4e8b6230eb331752105c29523766f9ca161
eliannaku25-meet <eliannabeatle@gmail.com>
2024-07-15 10:27:44 +0300

    og code

---
+++ 0.2 Python Review/python-practice.py
@@ -0,0 +1,37 @@
+
+def create_youtube_video(title, description):
+    youtube = {}
+    youtube["title"] = title
+    youtube["description"] = description
+    youtube["likes"] = 0
+    youtube["dislikes"] = 0
+    youtube["comments"] = {}
+    return youtube
+
+
+def dislike_video(video):
+    video["dislikes"] += 1
+    return video
+
+def add_comment(youtube_video, username, comment_text):
+    youtube_video["comments"][username] = comment_text
+    return youtube_video
+
+
+video = create_youtube_video("My First Video", "This is the description of my first video.")
+print("Initial video state:")
+print(video)
+
+
+video = dislike_video(video)
+print(video)
+
+video = add_comment(video, "user1", "Great video!")
+print(video)
+
+video = add_comment(video, "user2", "Nice job!")
+print(video)
+
+video ["likes"] = 465
+video = dislike_video(video)
+print(video)
