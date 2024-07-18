f7a75cc40614a867f14e64b674d1cc921a91a0ea
eliannaku25-meet <eliannabeatle@gmail.com>
2024-07-18 12:00:50 +0300

    finally!!!!

--- 0.6. Advanced Flask fortune-teller/genie.py
+++ 0.6. Advanced Flask fortune-teller/genie.py
@@ -1,21 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 import random
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
-
-@app.route('/home', methods = ['GET', 'POST'])
-def hello_world():
-	if request.method=='GET':
-		return render_template ('home.html')
-	else:
-		month= request.form['birthmonth']
-		return redirect(url_for('fortuneFinder',month=month))
-
-@app.route('/fortune/<month>')
-def fortuneFinder(month):
-	 
-    fortunes = {
+fortunes = {
     "January": "This year brings new beginnings and fresh starts. Embrace the changes.",
     "February": "Love and friendship will flourish this year. Keep an open heart.",
     "March": "New opportunities will come your way. Be ready to seize them.",
@@ -29,8 +17,27 @@ def fortuneFinder(month):
     "November": "Transformation and change are on the horizon. Be adaptable.",
     "December": "Joy and celebration will fill your days. Share your happiness."
 }
-    
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'GET':
+        return render_template('home.html')
+
+    input_month = request.form['birthmonth']
+    for x in fortunes.keys():
+        if x == input_month:
+            return redirect(url_for('fortuneFinder', month=input_month))
+
+    return redirect(url_for('error'))
+
+@app.route('/fortune/<month>')
+def fortuneFinder(month):
     return render_template("fortune.html", fortune=fortunes[month])
 
+@app.route('/error')
+def error():
+    return render_template("error.html")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
