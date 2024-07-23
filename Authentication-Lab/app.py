6aad7d95ede3c926ba9b5bdba4f3504949b6f479
eliannaku25-meet <eliannabeatle@gmail.com>
2024-07-23 17:51:17 +0300

    basic
    the code is running proporly without the contact page

--- Authentication-Lab/app.py
+++ Authentication-Lab/app.py
@@ -1,74 +1,98 @@
-from flask import Flask, render_template, request, redirect, url_for, session as login_session
+from flask import Flask, render_template, request, redirect, url_for
+from flask import session as login_session 
 import pyrebase
 
-# App configuration
-app = Flask(__name__, template_folder='templates', static_folder='static')
-app.config['SECRET_KEY'] = 'super-secret-key'
+app = Flask(__name__)
+app.config['SECRET_KEY']="*******"
 
-# Firebase configuration
+
 firebaseConfig = {
-  "apiKey": "AIzaSyAr6GBfiEUoSs7PU1Iz6RsXY973W3mpW-8",
-  "authDomain": "authlab-44360.firebaseapp.com",
-  "projectId": "authlab-44360",
-  "storageBucket": "authlab-44360.appspot.com",
-  "messagingSenderId": "436236602790",
-  "appId": "1:436236602790:web:253dfc8a1613048218d367",
-  "measurementId": "G-LJ60N5MB83",
-  "databaseURL": ""
+    "apiKey":"AIzaSyBSjFprdvxhVfWSEgdGUvlAbgylmKQkCWU",
+    "authDomain":"authentificationproject-4939c.firebaseapp.com",
+    "databaseURL":"https://authentificationproject-4939c-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId":"authentificationproject-4939c",
+    "storageBucket":"authentificationproject-4939c.appspot.com",
+    "messagingSenderId":"363635350388",
+    "appId":"1:363635350388:web:aaa00e201e9ed520d7ea21",
+    "measurementId": "G-GC8PC6HZG0",
+    "databaseURL": "https://authentificationproject-4939c-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-# Initialize Firebase
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
+db = firebase.database()
 
-# App routes
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template("login.html")
+        return render_template("login.html") 
     else: 
         email = request.form['email']
         password = request.form['password']
+        quotes = []
+        login_session['quotes']= quotes
+        login_session.modified = True
         try:
             login_session['user'] = auth.sign_in_with_email_and_password(email, password)
             return redirect(url_for('home'))
         except:
-            error = "Login failed, try again."
+            error = "Womp it failed sad"
             return render_template("login.html", error=error)
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
     if request.method == 'GET':
-        return render_template("signup.html")
-    else: 
+        login_session.modifed= True
+        return render_template("signup.html") 
+    else: #if the method is post
         email = request.form['email']
         password = request.form['password']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            print(login_session['user'])
+            print(login_session['user']['localId'])
+            login_session.modifed= True
             return redirect(url_for('home'))
         except:
-            error = "Authentication error"
-            return render_template("signup.html", error=error)
-
-@app.route('/home', methods=["GET", "POST"])
+            error = "Womp it failed. Try again"
+            login_session.modifed= True
+            return render_template("signup.html",error=error)
+        
+    
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    if request.method == "GET":
-        return render_template("home.html")
+    if request.method=='GET':
+        return render_template('home.html')
     else:
-        login_session['user'] = None
-        return redirect(url_for('login'))
+        print("Made it to line 65!")
+        login_session['quotes'].append(request.form['quote'])
+        db.child("users").child(user_id).set({
+            "user_quote": quote,
+            })
+        print(login_session['quotes'])
+        login_session.modified= True
+        return redirect(url_for('thanks'))
+
+
+@app.route('/signout')
+def signout():
+    login_session['user']=None
+    auth.current_user=None
+    login_session.modified= True
+    return redirect(url_for('login'))
 
 @app.route('/thanks')
 def thanks():
-    return render_template("thanks.html")
+    login_session.modified= True
+    return render_template('thanks.html')
 
-@app.route('/display', methods=['GET', 'POST'])
+@app.route('/display')
 def display():
-    if request.method == 'POST':
-        quote = request.form['quote']
-        my_quotes = [quote]  # This is a simple implementation, normally you would store quotes in a database.
-        return render_template("display.html", quote=my_quotes)
-    return render_template("display.html", quote=[])
+    quotes= db.child("users").child(user_id).child(user_id).get().val("user_quote": quote)
+        
+
+    login_session.modified = True
+    return render_template('display.html',quotes=quotes)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Lost&Found/lost.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask import session as login_session 
+import pyrebase
+
+app = Flask(__name__)
+app.config['SECRET_KEY']="*******"
+
+
+firebaseConfig = {  
+    "apiKey": "AIzaSyDA5G2lEmKiOFiup5ha-tfdQely23Q7Rto",
+    "authDomain": "lost-and-found-665f2.firebaseapp.com",
+    "projectId": "lost-and-found-665f2",
+    "storageBucket": "lost-and-found-665f2.appspot.com",
+    "messagingSenderId": "867865507512",
+    "appId": "1:867865507512:web:ed9276d47451a13c78a64d",
+    "measurementId": "G-69KTFNLTGT",
+    "databaseURL": "https://lost-and-found-665f2-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+db = firebase.database()
+
+@app.route('/', methods=['GET', 'POST'])
+
+def login():
+    if request.method == 'GET':
+        return render_template("login.html") 
+    else: 
+        email = request.form['email']
+        password = request.form['password']
+        quotes = []
+        login_session['quotes']= quotes
+        login_session.modified = True
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('home'))
+        except Exception as e:
+            error = "Womp it failed sad"
+            print(e)
+            return render_template("login.html", error=error)
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    if request.method == 'GET':
+        login_session.modifed= True
+        return render_template("signup.html") 
+    else: 
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            login_session['user']= user
+            user_id = user['localId']
+            print(login_session['user'])
+            print(login_session['user']['localId'])
+            login_session.modifed= True
+            return redirect(url_for('home'))
+        except Exception as e:
+            error = "Womp it failed. Try again"
+            print(e)
+            login_session.modifed= True
+            return render_template("signup.html",error=error)
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method=='GET':
+        return render_template('home.html')
+    
+@app.route('/lost', methods=['GET', 'POST'])
+def lost():
+    if request.method=='GET':
+        return render_template('lost.html')
+    else:
+            item = request.form['item']
+            item_description = request.form['item-description']
+            last_seen = request.form['last-seen']
+            user_id = login_session['user']['localId']
+            db.child("users").child(user_id).set({
+                    "item" : item,
+                    "item_description" : item_description,
+                    "last_seen" : last_seen
+                    })
+            user_id = login_session['user']['localId']
+            items = db.child("users").child(user_id).get().val()
+            return render_template('home.html', items=items, item=item,item_description=item_description,last_seen=last_seen)
+
+        
+if __name__ == '__main__':
+    app.run(debug=True)
